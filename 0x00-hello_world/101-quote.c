eb90461587d4e5dc53e7fa69a8c221346c62fc29
Okoli Ekene <khennyphresh@gmail.com>
2022-03-10 12:17:37 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * Return: 1
  */
 int main(void)
 {
-	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n", 58);
+	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
