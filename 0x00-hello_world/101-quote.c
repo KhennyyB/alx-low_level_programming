244250525a187a6470357e9efbdeb4fc0d6a2619
Okoli Ekene <khennyphresh@gmail.com>
2022-03-10 12:44:19 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
- char ch = 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n, 58';
+ char ch = 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n, s';
   
  putchar(ch);
 
