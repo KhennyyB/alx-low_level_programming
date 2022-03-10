8d1ffab7eddf99495246f1e2236fcc2cee938913
Okoli Ekene <khennyphresh@gmail.com>
2022-03-10 12:32:59 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@ int main(void)
 {
 
  // Get the character to be written
-    char ch = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
+    char ch = 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n, 58';
   
     // Write the Character to stdout
         putchar(ch);
