a619d3991c7ed597660bddd7485d170eaa0f6a5a
Okoli Ekene <khennyphresh@gmail.com>
2022-03-15 10:08:14 +0100

    add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
