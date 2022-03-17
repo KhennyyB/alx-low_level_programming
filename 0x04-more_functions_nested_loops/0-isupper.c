402835f134348a434919cf70f16cb7f0085f2fbd
Okoli Ekene <khennyphresh@gmail.com>
2022-03-17 12:36:24 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  *_isupper - function that checks for uppercase character
- *@c - int type number
+ *@c - character to check
  *Return: 1 if uppercase, 0 if otherwise
  */
 int _isupper(int c);
 {
-	if (c > 'A' && c < 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
+
 	return (0);
 }
