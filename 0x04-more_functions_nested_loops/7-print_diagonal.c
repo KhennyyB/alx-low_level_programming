104db6e310bb7f59109ed553135eab911517e0c7
Okoli Ekene <khennyphresh@gmail.com>
2022-03-17 21:06:16 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,23 @@
  */
 void print_diagonal(int n)
 {
-	int c;
+	int c, i;
 
 	c = 0;
-	while (c < n)
-	{
-		_putchar('\');
 
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
 		c++;
+		n--;
 	}
-	_putchar('\n');
+	if (c < 1)
+		_putchar('\n');
 }
