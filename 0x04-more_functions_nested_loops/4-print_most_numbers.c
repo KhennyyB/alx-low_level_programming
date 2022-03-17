7c7bc3101a93658f0da5548bdffa4294bf34fb14
Okoli Ekene <khennyphresh@gmail.com>
2022-03-17 14:39:45 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_most_numbers - function that prints the numbers 0-9, followed by a new line
+ * print_most_numbers - prints the numbers 0-9, followed by a new line
  * Description: Do not print '2' and '4', use _putchar only twice
  */
- void print_most_numbers(void)
+void print_most_numbers(void)
 {
 	int i = '0';
 
