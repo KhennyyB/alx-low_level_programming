11f8fc36b5333f3f1d8bc7ab18875e6792ae0f5a
Okoli Ekene <khennyphresh@gmail.com>
2022-03-23 21:21:16 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,12 +12,12 @@ char *cap_string(char *s)
 
 	i = 1;
 	if (s[0] > 96 && s[0] = 123)
-		s[0] -= (32);
+		s[0] -= 32;
 	while (s[i] != '\0')
 	{
 		for (j = 0; sep[j] != '\0'; j++)
 			if (s[i - 1] == sep[j] && (s[i] > 96 && s[i] < 123))
-				s[i] -= (32);
+				s[i] -= 32;
 		i++;
 	}
 	return (s);
