4fc3a53ade4cbcb6d754ac2ba62b44a300f8e873
Okoli Ekene <khennyphresh@gmail.com>
2022-03-23 17:14:22 +0100

    Create main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef main
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +11,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+
+#define main
+#endif
