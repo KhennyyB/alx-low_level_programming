2b26b80426bd2d981b3cd7115a5f92d920ace36b
Okoli Ekene <khennyphresh@gmail.com>
2022-04-01 18:29:10 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
 	(void) argc;
 
 	if (argv[1] && argv[2])
+argc == 3;
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
 		printf("Error\n");
-
 	return (1);
 }
