e0d47e0548d73ec3ce3027185d7a8d4412e2e97a
Okoli Ekene <khennyphresh@gmail.com>
2022-04-11 15:00:57 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,38 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
- *  * new_dog - create a new dog
- *   * @name: char string name
- *    * @age: int age
- *     * @owner: char string owner
- *      * Return: pointer to new dog
- *       */
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer to buffer of datatype dog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-		struct dog *doge;
-			int i, j, k;
-				char *n, *o;
+	int nlen, olen, i;
+	dog_t *doggy;
 
-					doge = malloc(sizeof(struct dog));
-						if (doge == NULL)
-									return (NULL);
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
 
-							for (i = 0; name[i] != '\0'; i++)
-										;
-								for (j = 0; owner[j] != '\0'; j++)
-											;
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
 
-									n = malloc(sizeof(char) * i + 1);
-										if (n == NULL)
-												{
-															free(doge);
-																	return (NULL);
-																		}
-											o = malloc(sizeof(char) * j + 1);
-												if (o == NULL)
-														{
-																	free(n);
-																			free(doge);
-																					return (NULL);
-																						}
-													for (k = 0; k <= i; k++)
-																n[k] = name[k];
-														for (k = 0; k <= j; k++)
-																	o[k] = owner[k];
+	doggy->age = age;
 
-															doge->name = n;
-																doge->age = age;
-																	doge->owner = o;
-
-																		return (doge);
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
 }
