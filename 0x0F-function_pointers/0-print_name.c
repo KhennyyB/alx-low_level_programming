7feb97e354454b689325a0b8defe1a738ce769fd
Okoli Ekene <khennyphresh@gmail.com>
2022-04-14 00:47:34 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - prints a name
-  * @name: input string
-  * @f: function pointer casted to char pointer
-  */
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	f(name);
 }
