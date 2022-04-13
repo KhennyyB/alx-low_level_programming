72ffb383520d45c12db4c27f9e5bf78c9c168985
Okoli Ekene <khennyphresh@gmail.com>
2022-04-14 00:39:39 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,24 +1,41 @@
+#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "3-calc.h"
+
 /**
-  * main - define function
-  * @argc: count of arguments
-  * @argv: pointer to array of pointers
-  * Return: 0 on success, 98 on malformed arguments, 99 if no operator
-  */
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int a, b;
+	int (*o)(int, int);
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	if (get_op_func(argv[2]) == NULL)
-		printf("Error\n"), exit(99);
-	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
-		printf("Error\n"), exit(100);
-	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = get_op_func(argv[2]);
+	if (o == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", o(a, b));
+
 	return (0);
 }
